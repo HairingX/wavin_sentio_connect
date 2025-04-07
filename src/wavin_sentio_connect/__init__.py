b1a5f70968733678db7a8886fffc6d4cdad8f906
patrick <patrickhairingdahl@gmail.com>
2025-04-07 22:12:51 +0200

    add missing code

--- src/wavin_sentio_connect/__init__.py
+++ src/wavin_sentio_connect/__init__.py
@@ -1,11 +1,23 @@
 from .devices import *
 from .wavin_sentio_connect import ( WavinSentioTCPConnect )
-from modbus_event_connect import ( ModbusExceptCode, ModbusTCPErrorCode )
+from modbus_event_connect import (  ModbusExceptCode,
+                                    ModbusTCPErrorCode,
+                                    ModbusPointKey,
+                                    ModbusDatapointKey,
+                                    ModbusSetpointKey,
+                                    MODBUS_VALUE_TYPES ,
+                                    UOM,
+                                    )
 
 __version__ = "0.1.0"
 __all__ = [
     "ModbusExceptCode",
     "ModbusTCPErrorCode",
+    "ModbusPointKey",
+    "ModbusDatapointKey",
+    "ModbusSetpointKey",
+    "MODBUS_VALUE_TYPES",
+    "UOM",
     "WavinSentioTCPConnect",
     "WavinSentio",
     "WavinSentioDatapointKey",
